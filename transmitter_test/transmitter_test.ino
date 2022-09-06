b2e576565c735eb2ec8050b60b7fe38ac5306ebb
mumslayer69 <81803537+mumslayer69@users.noreply.github.com>
2022-09-06 12:52:05 +0800

    Update transmitter_test.ino

--- transmitter_test/transmitter_test.ino
+++ transmitter_test/transmitter_test.ino
@@ -3,31 +3,35 @@
 #include <RF24.h>
 
 #define TX_DELAY 10
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(10, 9); // CE, CSN
 
 uint8_t address[][6] = {"send", "reci"};
 
-int writeval = 69;
+int writeval[5] = {0, 690, 420, 1337, 9001};
+// int writeval = 69;
 
 void setup() {
   Serial.begin(19200);
   radio.begin();
   radio.openWritingPipe(address[1]);
-  radio.setPayloadSize(sizeof(int));
+  radio.setPayloadSize(5 * sizeof(int));
   radio.setPALevel(RF24_PA_LOW);
   radio.stopListening();
   Serial.println("initialising transmitter");
-}
+}   
 
 void loop() {
-  radio.write(&writeval, sizeof(writeval));
-  // Serial.print("sending ");
-  // Serial.println(writeval);
-  writeval++;
+  radio.write(&writeval, 5 * sizeof(writeval));
+  
+  Serial.println("sending ");
   /*
-  if(TX_DELAY) {
-    delay(TX_DELAY);
-  }
+  Serial.print(writeval[0]);
+  Serial.print(writeval[1]);
+  Serial.print(writeval[2]);
+  Serial.print(writeval[3]);
+  Serial.print(writeval[4]);
+  Serial.println();
   */
-  delay(10);
+  // writeval++;
+  delay(TX_DELAY);
 }
