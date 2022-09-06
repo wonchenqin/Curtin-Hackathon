fa540b41f473f1a2ec89fbb1f461324097e9df0f
Iceman3557 <45889395+Iceman3557@users.noreply.github.com>
2022-09-06 16:24:56 +0800

    Update receiver_test_final.ino

--- receiver_test_final/receiver_test_final.ino
+++ receiver_test_final/receiver_test_final.ino
@@ -6,43 +6,38 @@
 * 
 * Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
+long int ii;
+int counter;
 
-RF24 radio(A4, A5); // CE, CSN
-
-uint8_t address[][6] = {"send", "reci"};
-
-int readval[5];
-// int readval;
-
-void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(1, address[1]);
-  radio.setPayloadSize(5 * sizeof(int));
-  radio.setPALevel(RF24_PA_LOW);
-  radio.startListening();
-  Serial.println("initialising receiver");
+void setup(){
+    pinMode(LEDPIN, OUTPUT);
+  ii = 0;
+  counter = millis();
 }
 
 void loop() {
-  if (radio.available()) {
-  radio.read(&readval, 5 * sizeof(int));
-  Serial.print(readval[0]);
-  Serial.print(" ");
-  Serial.print(readval[1]);
-  Serial.print(" ");
-  Serial.print(readval[2]);
-  Serial.print(" ");
-  Serial.print(readval[3]);
-  Serial.print(" ");
-  Serial.print(readval[4]);
-  Serial.println();
-  }
-  else {
-    // Serial.println("radio not available");
-  }
-  delay(20);
+
+  ledCode();
+}
+void ledCode() {
+
+
+    if (LEDCODE[ii%16] == 1)
+    {
+      digitalWrite(LEDPIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(LEDPIN, LOW);
+    }
+    if (millis() > counter + 100)
+    {
+      ii = ii + 1;
+      counter = millis();
+    }
+
+    if(ii >= 10000){
+      ii =0;
+    }
+
 }
